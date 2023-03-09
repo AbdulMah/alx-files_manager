9e4b132ea4e709e236d4b40a676b038898f6ae05
Abdoulkarim <amahamn@gmail.com>
2023-03-10 00:54:24 +0100

    final

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import AppController from "../controllers/AppController";
 import AuthController from "../controllers/AuthController";
 import UsersController from "../controllers/UsersController";
 import FilesController from "../controllers/FilesController";
-import { basicAuthenticate, xTokenAuthenticate } from "../middlewares/auth";
+import { basicAuthenticate, TokenAuthenticate } from "../middlewares/auth";
 import { APIError, errorResponse } from "../middlewares/error";
 
 /**
@@ -13,32 +13,54 @@ import { APIError, errorResponse } from "../middlewares/error";
  */
 
 const routes = (app) => {
-  const router = express.Router();
+  /*   const router = express.Router();
   app.use("/", router);
 
   router.get("/status", AppController.getStatus);
   router.get("/stats", AppController.getStats);
 
   router.get("/connect", basicAuthenticate, AuthController.getConnect);
-  router.get("/disconnect", xTokenAuthenticate, AuthController.getDisconnect);
+  router.get("/disconnect", TokenAuthenticate, AuthController.getDisconnect);
 
   router.post("/users", UsersController.postNew);
-  router.get("/users/me", xTokenAuthenticate, UsersController.getMe);
+  router.get("/users/me", TokenAuthenticate, UsersController.getMe);
 
-  router.post("/files", xTokenAuthenticate, FilesController.postUpload);
-  router.get("/files/:id", xTokenAuthenticate, FilesController.getShow);
-  router.get("/files", xTokenAuthenticate, FilesController.getIndex);
+  router.post("/files", TokenAuthenticate, FilesController.postUpload);
+  router.get("/files/:id", TokenAuthenticate, FilesController.getShow);
+  router.get("/files", TokenAuthenticate, FilesController.getIndex);
   router.put(
     "/files/:id/publish",
-    xTokenAuthenticate,
+    TokenAuthenticate,
     FilesController.putPublish
   );
   router.put(
     "/files/:id/unpublish",
-    xTokenAuthenticate,
+    TokenAuthenticate,
     FilesController.putUnpublish
   );
   router.get("/files/:id/data", FilesController.getFile);
+
+ */
+
+  app.get("/status", AppController.getStatus);
+  app.get("/stats", AppController.getStats);
+
+  app.get("/connect", basicAuthenticate, AuthController.getConnect);
+  app.get("/disconnect", TokenAuthenticate, AuthController.getDisconnect);
+
+  app.post("/users", UsersController.postNew);
+  app.get("/users/me", TokenAuthenticate, UsersController.getMe);
+
+  app.post("/files", TokenAuthenticate, FilesController.postUpload);
+  app.get("/files/:id", TokenAuthenticate, FilesController.getShow);
+  app.get("/files", TokenAuthenticate, FilesController.getIndex);
+  app.put("/files/:id/publish", TokenAuthenticate, FilesController.putPublish);
+  app.put(
+    "/files/:id/unpublish",
+    TokenAuthenticate,
+    FilesController.putUnpublish
+  );
+  app.get("/files/:id/data", FilesController.getFile);
 
   app.all("*", (req, res, next) => {
     errorResponse(
